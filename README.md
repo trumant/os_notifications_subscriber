3f85ab6d0d9e5e43f57b1c4aa2081b6cb38e0ba5
Travis Truman <travis_truman@cable.comcast.com>
2016-08-15 17:06:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# OpenStack Notifications Subscriber
+
+Lightweight subscriber to OpenStack notification messages.
+
+## Installation
+
+```shell
+pip install virtualenv
+git clone https://github.com/trumant/os_notification_subscriber /opt/os_notifications_subscriber
+virtualenv /opt/os_notifications_subscriber
+cd /opt/os_notifications_subscriber
+source bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+```shell
+source bin/activate
+python subscribe.py
+```
---
+++ subscribe.py
@@ -0,0 +1,47 @@
+# Adapted from
+# http://alesnosek.com/blog/2015/05/25/openstack-nova-notifications-subscriber/
+
+import logging as log
+import os
+import sys
+
+from kombu import BrokerConnection
+from kombu import Exchange
+from kombu import Queue
+from kombu.mixins import ConsumerMixin
+
+EXCHANGE_NAME = os.environ.get("OS_EXCHANGE", "nova")
+ROUTING_KEY = os.environ.get("OS_ROUTING_KEY", "notifications.info")
+QUEUE_NAME = "dump_queue"
+BROKER_USER = os.environ.get("RABBIT_USERNAME", "guest")
+BROKER_PASSWORD = os.environ.get("RABBIT_PASSWORD", "guest")
+BROKER_HOST = os.environ.get("RABBIT_HOST", "localhost")
+BROKER_PORT = os.environ.get("RABBIT_PORT", "5672")
+BROKER_VHOST = os.environ.get("RABBIT_VHOST", "/")
+BROKER_URI = "amqp://{0}:{1}@{2}:{3}/{4}".format(
+    BROKER_USER, BROKER_PASSWORD, BROKER_HOST, BROKER_PASSWORD,
+    BROKER_PORT, BROKER_VHOST)
+
+log.basicConfig(stream=sys.stdout, level=log.DEBUG)
+
+
+class NotificationsDump(ConsumerMixin):
+
+    def __init__(self, connection):
+        self.connection = connection
+        return
+
+    def get_consumers(self, consumer, channel):
+        exchange = Exchange(EXCHANGE_NAME, type="topic", durable=False)
+        queue = Queue(QUEUE_NAME, exchange, routing_key=ROUTING_KEY,
+                      durable=False, auto_delete=True, no_ack=True)
+        return [consumer(queue, callbacks=[self.on_message])]
+
+    def on_message(self, body, message):
+        log.info('Body: %r' % body)
+        log.info('---------------')
+
+if __name__ == "__main__":
+    log.info("Connecting to broker {}".format(BROKER_URI))
+    with BrokerConnection(BROKER_URI) as connection:
+        NotificationsDump(connection).run()
